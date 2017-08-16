d467fe153dcf3088e9f216a1978b3e8d8a12a5e2
zhangjiayi <zhangjiayi@haohushi.me>
2017-08-16 15:07:03 +0800

    1.0

---
+++ XMGStatusBarHUDDemo/XMGStatusBarHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMGStatusBarHUD
+//
+//  Created by zhangjiayi on 2017/8/16.
+//  Copyright © 2017年 YiXinKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
